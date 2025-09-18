a33bd01d26a2e7b0b32d6e5234519d24f1a100e3
Maik Rodriguez <maikrp@gmail.com>
2025-09-18 17:07:59 -0600

    Uso variables de entorno para Supabase

--- src/utils/supabase.js
+++ src/utils/supabase.js
@@ -1,8 +1,6 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
 
-// URL y anon key reales de tu proyecto Supabase
-const supabaseUrl = 'https://plarayvxtwdebiotsmmd.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBsYXJheXl3dHhlZGJpb3RzbW1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTgxMzQ0NTQsImV4cCI6MjA3MzcxMDQ1NH0.s585WUBDWj9F3O9r5c_mzUTdPGbpSFhez2FgJhyya9w' // tu anon key
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-// Cliente exportado para usar en toda la app
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(supabaseUrl, supabaseKey);
